7f0c0f7992ced5ceee576a1e88035020d450b077
caterpilerr <caterpilerr@gmail.com>
2020-07-31 22:42:56 +0300

    created random and search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'encyclopedia'
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('search', views.search, name='search'),
+    path('random', views.random, name='random'),
     path('<str:title>', views.entry, name='entry'),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,9 @@
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render
 
+from random import Random
 from markdown2 import Markdown
 from . import util
 
@@ -9,6 +12,21 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def search(request):
+    entries = util.list_entries()
+    title = request.GET["search"]
+    if title:
+        if title in entries:
+            return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={'title': title}))
+        else:
+            matches = [entry for entry in entries if title in entry.lower() ]
+            return render(request, "encyclopedia/search.html", {
+                "search": title,
+                "matches": matches
+            })
+    return HttpResponseRedirect(reverse("encyclopedia:index"))
+                
 
 def entry(request, title):
     content = util.get_entry(title)
@@ -21,4 +39,7 @@ def entry(request, title):
         "content": content
     })
 
+def random(request):
+    title = Random().choice(util.list_entries())
+    return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={'title': title}))
 
