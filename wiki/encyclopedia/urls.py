1c42ba788384d5e3dfb215db76b074f73e17d39a
Victor Belokashin <caterpilerr@gmail.com>
2020-08-17 17:48:06 +0300

    added creating of new pages

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search', views.search, name='search'),
     path('random', views.random, name='random'),
+    path('new_entry', views.new_entry, name='new_entry'),
     path('<str:title>', views.entry, name='entry'),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
@@ -15,14 +16,14 @@ def index(request):
 
 def search(request):
     entries = util.list_entries()
-    title = request.GET["search"]
-    if title:
-        if title in entries:
-            return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={'title': title}))
+    query = request.GET["search"]
+    if query:
+        if query in entries:
+            return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={'title': query}))
         else:
-            matches = [entry for entry in entries if title in entry.lower() ]
+            matches = [entry for entry in entries if query.lower() in entry.lower()]
             return render(request, "encyclopedia/search.html", {
-                "search": title,
+                "search": query,
                 "matches": matches
             })
     return HttpResponseRedirect(reverse("encyclopedia:index"))
@@ -31,10 +32,11 @@ def search(request):
 def entry(request, title):
     content = util.get_entry(title)
     if not content:
-        raise Http404
-    md_converter = Markdown()
-    content = md_converter.convert(content)
-    return render(request, 'encyclopedia/entry.html', {
+        content = f"#{title}\n Wiki doesn't have article with this exact name."
+    else:    
+        md_converter = Markdown()
+        content = md_converter.convert(content)
+    return render(request, "encyclopedia/entry.html", {
         "title": title,
         "content": content
     })
@@ -42,4 +44,17 @@ def entry(request, title):
 def random(request):
     title = Random().choice(util.list_entries())
     return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={'title': title}))
+
+
+def new_entry(request):
+    if request.method == 'POST':
+        title = request.POST["title"]
+        content = request.POST["page_content"].encode()
+        if title:
+            entries = util.list_entries()
+            if not title in entries:
+                util.save_entry(title, content)
+            else:
+                messages.error(request, 'Page with same title already exists!')
+    return render(request, "encyclopedia/new_entry.html")
 
